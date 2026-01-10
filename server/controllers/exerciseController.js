e4f6a62375df2b65cd6bb51ddf9013066360a6dd
Riddhesh Kataria <riddheshkataria@gmail.com>
2026-01-10 23:53:57 +0530

    Exercise controller bug solved

--- server/controllers/exerciseController.js
+++ server/controllers/exerciseController.js
@@ -3,7 +3,7 @@ import Exercise from '../models/Exercise.js'
 export const getExercises= async(req, res)=>{
     try{
         const exercises= await Exercise.find({});
-        res.status(200).json();
+        res.status(200).json(exercises);
     }   catch (error){
         res.status(500).json({message: "Failed to fetch exercises"});
     }
