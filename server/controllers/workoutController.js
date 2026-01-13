f71741e690fb3c2351957bb11ad504253f4bc979
Riddhesh Kataria <riddheshkataria@gmail.com>
2026-01-13 23:28:05 +0530

    ongoing workout 'Add workout controllers and active session tracking

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -2,11 +2,33 @@ import Workout from "../models/Workout.js";
 
 export const startWorkout= async(req,res) =>{
     try{
+        const existingWorkout = await Workout.findOne({ 
+            user: req.user._id, 
+            status: 'ongoing' 
+        });
+        
+        if (existingWorkout) {
+            return res.status(400).json({ message: "You already have an active workout!" });
+        }
+
         const workout= await Workout.create({
             user:req.user._id,
             exercises:[]
         });
+        res.status(201).json(workout);
     }catch(error){
         res.status(500).json({message: "Failed to start workout"});
+    }
+};
+
+export const getOngoingWorkout =async(req,res)=>{
+    try{
+        const workout= await Workout.findOne({
+            user:req.user._id,
+            status:'ongoing'
+        }).populate('exercises.exerciseId');
+        res.status(200).json(workout);
+    } catch(error){
+        res.status(500).json({message: "Error fetching the ongoing workout"});
     }
 };
--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { startWorkout } from '../controllers/workoutController.js';
+import { startWorkout , getOngoingWorkout} from '../controllers/workoutController.js';
 import { protect } from '../middleware/authMiddleware.js'; // Import the guard
 
 const router = express.Router();
     router.route('/').post(protect, startWorkout);
-
+    router.get('/ongoing',protect,getOngoingWorkout);
 export default router;
