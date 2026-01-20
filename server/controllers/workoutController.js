9b4b9c615215ee8d22be803839a42a606a4f94ae
Riddhesh Kataria <riddheshkataria@gmail.com>
2026-01-20 08:19:08 +0530

    Created addExerciseToWorkout to push new exercises to the workout and finishWorkout to save it. (with routes)

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -32,3 +32,50 @@ export const getOngoingWorkout =async(req,res)=>{
         res.status(500).json({message: "Error fetching the ongoing workout"});
     }
 };
+
+export const addExerciseToWorkout= async(req,res)=>{
+    try{
+        const workout= await Workout.findById(req.params.id);
+
+        if (!workout) return res.status(404).json({ message: "Workout not found" });
+        const newExercise={
+            exerciseId:req.body.exerciseId,
+            sets:[{weight:0, reps:0}]
+        };
+
+        if (workout.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Not authorized to edit this workout" });
+        }
+
+        workout.exercises.push(newExercise);
+        await workout.save();
+
+        const updatedWorkout = await Workout.findById(workout._id).populate('exercises.exerciseId');
+        res.status(200).json(updatedWorkout);
+    }   catch(error){
+        res.status(500).json({message: "Failed to add exercise"});
+    }
+};
+
+export const finishWorkout= async(req,res)=>{
+    try{
+        const workout= await Workout.findById(req.params.id);
+        
+        if (!workout) {
+            return res.status(404).json({ message: "Workout not found" });
+        }
+
+        if (workout.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ message: "Not authorized" });
+        }
+
+        workout.status = 'completed';
+        workout.finishedAt = Date.now();
+        
+        await workout.save();
+        res.status(200).json({ message: "Workout completed successfully", workout });
+
+    } catch(error){
+        res.status(500).json({message: "Failed to finish workout"});
+    }
+};
--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { startWorkout , getOngoingWorkout} from '../controllers/workoutController.js';
-import { protect } from '../middleware/authMiddleware.js'; // Import the guard
+import { 
+    startWorkout, 
+    getOngoingWorkout,
+    addExerciseToWorkout,
+    finishWorkout
+} from '../controllers/workoutController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
     router.route('/').post(protect, startWorkout);
     router.get('/ongoing',protect,getOngoingWorkout);
+    router.route('/:id/add').put(protect, addExerciseToWorkout);
+    router.route('/:id/finish').put(protect, finishWorkout);
 export default router;
